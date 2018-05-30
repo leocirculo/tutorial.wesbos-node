bfb7c2d820e8cb8c741431a0ec3b8c248ad0091b
Leo Gonzalez <lasociedadloka@gmail.com>
2018-05-30 19:41:10 -0300

    agregado login

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('./../controllers/storeController');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -28,11 +29,14 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
 
-// 1. validate registration
-
+router.post('/register', 
+  // 1. validate registration
+  userController.validateRegister,
+  userController.register,
+  authController.login,
+);
 // 2. register the user
 // 3. we need to log them in
-router.post('/register', userController.validateRegister);
 
 
 module.exports = router;
