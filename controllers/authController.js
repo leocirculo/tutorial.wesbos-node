7d975a4db96ccb35416f2a9ebe51dfb21a42be37
Leo Gonzalez <lasociedadloka@gmail.com>
2018-06-22 19:48:59 -0300

    creando node mailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('./../handlers/mail');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: 'login',
@@ -36,7 +37,14 @@ exports.forgot = async (req, res) => {
   await user.save();
   // 3. Send them an email with the token
   const resetUrl = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-  req.flash('success', `You have been emailed a password reset link . ${resetUrl}`);
+
+  await mail.send({
+    user,
+    subject: 'Password Reset',
+    resetUrl,
+  });
+
+  req.flash('success', `You have been emailed a password reset link`);
   // 4. Redirect to login page
   res.redirect('/login');
 };
